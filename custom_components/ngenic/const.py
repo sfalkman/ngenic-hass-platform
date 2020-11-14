2e73c3f226f2bba05f094ad029d199790baddd43
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2020-11-14 07:03:42 +0100

    improve readability

--- custom_components/ngenic/const.py
+++ custom_components/ngenic/const.py
@@ -9,4 +9,4 @@ How often to re-scan sensor information.
 From API doc: Tune system Nodes generally report data in intervals of five 
 minutes, so there is no point in polling the API for new data at a higher rate.
 """
-SCAN_INTERVAL = timedelta(seconds=(60 * 5))
+SCAN_INTERVAL = timedelta(minutes=5)
